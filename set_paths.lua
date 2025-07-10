c71b931767328fd345a8ff1ca207118b73d46d57
toasted323 <188188060+toasted323@users.noreply.github.com>
2025-07-10 19:28:56 +0200

    fix(lua): make Lua path separator portable in Lua set_paths helper

--- set_paths.lua
+++ set_paths.lua
@@ -1,4 +1,17 @@
 local version = _VERSION:match("%d+%.%d+")
-package.path = "./src/?.lua;./src/?/init.lua;lua_modules/share/lua/" .. version .. "/?.lua;lua_modules/share/lua/" ..
-                   version .. "/?/init.lua;" .. package.path
-package.cpath = "lua_modules/lib/lua/" .. version .. "/?.so;" .. package.cpath
+local path_sep = package.config and package.config:sub(3,3) or ';'
+
+local paths = {
+    "./src/?.lua",
+    "./src/?/init.lua",
+    "lua_modules/share/lua/" .. version .. "/?.lua",
+    "lua_modules/share/lua/" .. version .. "/?/init.lua",
+    package.path
+}
+package.path = table.concat(paths, path_sep)
+
+local cpaths = {
+    "lua_modules/lib/lua/" .. version .. "/?.so",
+    package.cpath
+}
+package.cpath = table.concat(cpaths, path_sep)
